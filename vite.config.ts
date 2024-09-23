63530cb39ab0a55e3dc55a6c6594f36a7dafab62
guscarpim <gustavoscarpim@gmail.com>
2024-09-22 22:54:09 -0300

    feat(caju): :rocket: update config paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslintPlugin()],
-  server: {
-    port: 3001,
-  },
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "src"),
+export default defineConfig((props) => {
+  const env = loadEnv(props.mode, process.cwd(), "VITE_DEV");
+  const envWithProcessPrefix = {
+    "process.env": `${JSON.stringify(env)}`,
+  };
+
+  return {
+    plugins: [react(), eslintPlugin()],
+    server: {
+      port: 3001,
     },
-  },
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
+    define: envWithProcessPrefix,
+    build: {
+      outDir: 'dist',
+    }
+  }
 });
